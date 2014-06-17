bb52ef44b4d50292b20f710c71a761feaba2a62c
Robert Montanez <rmontanezjr@a>
2014-06-17 12:42:42 -0500

    Added new book.

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+# R Script to do the following
+# 1) Merges the training and test sets to create one data set
+# 2) Extracts only the measurements on the mean and standard 
+#    deviation for each meaurmenet
+# 3) Uses descriptive activity names to name the activites
+#    in the data set
+# 4) Appropriately labels the data set with descriptive variable naems
+# 5) Creates a second, independent tidy data set with the average of each
+#    variable for each activity and each subect.
+#
+#
+#
+#
+
